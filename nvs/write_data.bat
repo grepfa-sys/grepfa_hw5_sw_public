bc3a4a50efdd88f3944efdc5bcfe2ec66d830d5e
YoungSoo Shin <ys@grepfa.com>
2023-11-06 18:54:53 +0900

    ADD NVS flash writer

--- nvs/write_data.bat
+++ nvs/write_data.bat
@@ -1,2 +1,2 @@
 python ./tool/nvs_partition_gen.py generate nvs.csv nvs_part.bin 16384
-python ./tool/parttool.py --port COM11 write_partition --partition-type=data --partition-subtype=nvs --input "nvs_part.bin"
+python ./tool/parttool.py write_partition --partition-type=data --partition-subtype=nvs --input "nvs_part.bin"
