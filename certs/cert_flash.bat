a78253843a067a1a1f494ad75938d080298c9717
YoungSoo Shin <ys@grepfa.com>
2023-11-06 18:55:39 +0900

    EDIT bash to windows bat

---
+++ certs/cert_flash.bat
@@ -0,0 +1,1 @@
+python C:\Users\sys0011\AppData\Local\Programs\Python\Python312\Scripts\configure_esp_secure_cert.py -p COM11   --keep_ds_data_on_host   --efuse_key_id 1   --ca-cert ca.pem   --device-cert client.crt   --private-key private.key   --target_chip esp32s3   --secure_cert_type cust_flash_tlv   --configure_ds   --priv_key_algo RSA 2048
