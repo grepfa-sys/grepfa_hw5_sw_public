325f88682ff93e03c873fb136a05653b1fdee129
YoungSoo Shin <ys@grepfa.com>
2023-11-09 01:17:35 +0900

    DEL unused files nvs directory merged factory directory

--- nvs/write_data.bat
+++
@@ -1,2 +0,0 @@
-python ./tool/nvs_partition_gen.py generate nvs.csv nvs_part.bin 16384
-python ./tool/parttool.py write_partition --partition-type=data --partition-subtype=nvs --input "nvs_part.bin"
